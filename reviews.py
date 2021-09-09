f85ee2a4b9bafdae37818f1184ddc407b56aebee
Simon <chaochiyang@gmail.com>
2021-09-09 20:18:25 +0800

    add length filter

--- reviews.py
+++ reviews.py
@@ -11,10 +11,16 @@ print('檔案讀取完了，總共有', len(data), '筆資料')
 
 
 sum_len = 0
-for d in data:     # 把 data 清單中的每筆留言一筆筆取出來排好成為 d 
+for d in data:     # 把 data 清單中的每筆留言一筆筆取出來成為 d （每筆留言是一個d） 
 	sum_len = sum_len + len(d)      # 所有留言的長度總和（迴圈依照順序加總到最後一筆）
 
 print('平均留言長度為', sum_len/len(data))
 
 
-
+new = [] 
+for d in data:
+	if len(d) < 100:
+		new.append(d) 
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
